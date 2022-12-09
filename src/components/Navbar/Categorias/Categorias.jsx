5decbe5c205d3de0a2533595185653bc68967075
Nicolás Abraham <nicoabr@gmail.com>
2022-12-09 00:00:42 -0300

    PreEntrega2 + Abraham v1.0

--- src/components/Navbar/Categorias/Categorias.jsx
+++ src/components/Navbar/Categorias/Categorias.jsx
@@ -5,7 +5,7 @@ const Categorias = () => {
         <li className="nav-item dropdown">
         <a className="menu nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Productos</a>
         <div className="dropdown-menu">
-            <a className="dropdown-item" href="">elemental</a>
+            <a className="dropdown-item" href="">Elemental</a>
             <a className="dropdown-item" href="">Veraniego</a>
             <a className="dropdown-item" href="">Otoñal</a>
         </div>
